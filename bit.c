f5b94b50a428274f1a8e611a9dc9916167afe7b9
tenalinandini22wh1a1244 <121919152+tenalinandini22wh1a1244@users.noreply.github.com>
2023-01-11 12:03:50 +0530

    Add files via upload

---
+++ bit.c
@@ -0,0 +1,9 @@
+#include<stdio.h>
+void main()
+{
+int a,b;
+printf("enter two values");
+scanf("%d %d", &a, &b);
+printf("the leftshift of a is %d\n", a<<3);
+printf("the rightshift of b is %d\n", b>>3);
+}
