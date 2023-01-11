81eadb04616168bd9deaa1d8a0f6eb2d8794cb4c
tenalinandini22wh1a1244 <121919152+tenalinandini22wh1a1244@users.noreply.github.com>
2023-01-11 09:39:07 +0530

    Add files via upload

---
+++ add.c
@@ -0,0 +1,10 @@
+//Program to divide two numbers
+#include<stdio.h>
+void main()
+{
+int n1 =30, n2=30;
+int division;
+division=n1/n2;
+printf("the division of %d and %d is %d", n1, n2, division);
+}
+
