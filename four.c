50c6ce887dc6e1bbb9e9d78d50f0a6cd3036e34d
tenalinandini22wh1a1244 <121919152+tenalinandini22wh1a1244@users.noreply.github.com>
2023-01-11 12:09:28 +0530

    Add files via upload

---
+++ four.c
@@ -0,0 +1,13 @@
+//c program for if-else
+#include<stdio.h>
+void main()
+{
+int n1;
+printf("enter a number n1");
+scanf("%d",&n1);
+if(n1/2==0)
+printf("%d is even", n1);
+else
+printf("%d is odd", n1);
+}
+
